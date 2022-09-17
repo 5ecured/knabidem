ecc02e1ce6b9374f988bafd7ac813ed8e7a63611
Edward <onirotto7585@pm.me>
2022-09-17 18:57:41 +1000

    logic done

--- src/components/DisplayContent.js
+++ src/components/DisplayContent.js
@@ -8,54 +8,57 @@ const DisplayContent = () => {
     useEffect(() => {
         const fetchData = async () => {
             const URL = 'https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json'
-            const { data } = await axios.get(URL)
-            setData(data)
+            const { data: result } = await axios.get(URL)
+
+            //To filter only people who have pets, as not everyone has pets
+            const peopleWithPets = result.filter(obj => obj.pets)
+
+            setData(peopleWithPets)
         }
+
         fetchData()
     }, [])
 
-    //To filter only people who have pets, as not everyone has pets
-    let peopleWithPets = []
-    data.forEach(obj => {
-        if (obj.pets) peopleWithPets.push(obj)
-    })
 
     //As per the challenge requirement - to list sorted cat names under the gender of their owner
-    let catsObjWithMaleOwner = [] // [ {cat object}, {cat object}, {cat object} ]
-    let catsObjWithFemaleOwner = [] // [ {cat object}, {cat object}, {cat object} ]
+    const populateCatsBasedOnGender = (array, gender) => {
+        let temp = []
 
-    peopleWithPets.forEach(el => {
-        if (el.gender === 'Male') {
+        const genderArray = array.filter(el => el.gender === gender)
+
+        genderArray.forEach(el => {
             el.pets.forEach(pet => {
-                if (pet.type === 'Cat') catsObjWithMaleOwner.push(pet)
+                if (pet.type === 'Cat') temp.push(pet)
             })
-        }
-        if (el.gender === 'Female') {
-            el.pets.forEach(pet => {
-                if (pet.type === 'Cat') catsObjWithFemaleOwner.push(pet)
-            })
-        }
-    })
+        })
+
+        return temp
+    }
 
 
-    let catsMaleArray = [] // e.g. ['Jim', 'Tom', 'Garfield']
-    catsObjWithMaleOwner.forEach(el => {
-        catsMaleArray.push(el.name)
-    })
+    //catsObjWithMaleOwner and catsObjWithFemaleOwner are now populated: [ {cat object}, {cat object}, {cat object} ]
+    const catsObjWithMaleOwner = populateCatsBasedOnGender(data, 'Male')
+    const catsObjWithFemaleOwner = populateCatsBasedOnGender(data, 'Female')
 
-    let catsFemaleArray = [] // e.g. ['Jim', 'Tom', 'Garfield']
-    catsObjWithFemaleOwner.forEach(el => {
-        catsFemaleArray.push(el.name)
-    })
+    //To produce an array of cat string names and sort them alphabetically 
+    const createAndSortCats = arr => {
+        let tempArray = []
+        arr.forEach(el => {
+            tempArray.push(el.name)
+        })
+        return tempArray.sort()
+    }
 
-    const sortedMaleCats = catsMaleArray.sort()
-    const sortedFemaleCats = catsFemaleArray.sort()
+    //Now we get an array of cat names that have been sorted e.g. [ "Garfield", "Jim", "Max", "Tom" ]
+    const catsMaleArray = createAndSortCats(catsObjWithMaleOwner)
+    const catsFemaleArray = createAndSortCats(catsObjWithFemaleOwner)
 
-    const displayMaleCats = sortedMaleCats.map(cat => (
+    //Turn the array into a JSX element, so that we can render 
+    const displayMaleCats = catsMaleArray.map(cat => (
         <li key={cat}>{cat}</li>
     ))
 
-    const displayFemaleCats = sortedFemaleCats.map(cat => (
+    const displayFemaleCats = catsFemaleArray.map(cat => (
         <li key={cat}>{cat}</li>
     ))
 

54f08d651b4b4befbe1e8a1cfdaf823bb9fd122d
Edward <onirotto7585@pm.me>
2022-09-18 19:22:17 +1000

    finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import DisplayContent from './components/DisplayContent'
-import CssBaseline from '@mui/material/CssBaseline';
-import { AppBar, Toolbar, Typography, Container } from '@mui/material';
-import { Button } from '@mui/material'
-import Grid from '@mui/material/Grid'
+import { AppBar, Toolbar, Typography, Container, Grid, Button, TextField, CssBaseline } from '@mui/material';
 
 const App = () => {
+  const [showSolution, setShowSolution] = useState(false)
+  const [fetchedData, setFetchedData] = useState([])
+  const [filteredData, setFilteredData] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const URL = 'https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json'
+      const { data: result } = await axios.get(URL)
+
+      //To filter only people who have pets, as not everyone has pets
+      const peopleWithPets = result.filter(obj => obj.pets)
+
+      setFetchedData(peopleWithPets)
+    }
+
+    fetchData()
+  }, [])
+
+
+
+  //As per the challenge requirement - to list sorted cat names under the gender of their owner
+  const populateCatsBasedOnGender = (array, gender) => {
+    let temp = []
+
+    const genderArray = array.filter(el => el.gender === gender)
+
+    genderArray.forEach(el => {
+      el.pets.forEach(pet => {
+        if (pet.type === 'Cat') temp.push(pet)
+      })
+    })
+
+    return temp
+  }
+
+
+
+  //catsObjWithMaleOwner and catsObjWithFemaleOwner are now populated: [ {cat object}, {cat object}, {cat object} ]
+  const catsObjWithMaleOwner = populateCatsBasedOnGender(fetchedData, 'Male')
+  const catsObjWithFemaleOwner = populateCatsBasedOnGender(fetchedData, 'Female')
+
+
+
+  //To produce an array of cat string names and sort them alphabetically 
+  const createAndSortCats = arr => {
+    let tempArray = []
+    arr.forEach(el => {
+      tempArray.push(el.name)
+    })
+    return tempArray.sort()
+  }
+
+
+
+  //Now we get an array of cat names that have been sorted e.g. [ "Garfield", "Jim", "Max", "Tom" ]
+  const catsMaleArray = createAndSortCats(catsObjWithMaleOwner)
+  const catsFemaleArray = createAndSortCats(catsObjWithFemaleOwner)
+
+
+
+  //For the filter feature to work
+  const filteredMaleArray = catsMaleArray.filter(el => (
+    el.toLowerCase().includes(filteredData.toLowerCase())
+  ))
+
+  const filteredFemaleArray = catsFemaleArray.filter(el => (
+    el.toLowerCase().includes(filteredData.toLowerCase())
+  ))
+
+
+
   return (
     <>
       <CssBaseline />
+
       <AppBar position='relative' color='error'>
         <Toolbar>
           <Grid container justifyContent='center' alignItems='center'>
@@ -18,27 +88,61 @@ const App = () => {
           </Grid>
         </Toolbar>
       </AppBar>
+
       <Container style={{ marginTop: '25px' }}>
         <Grid container justifyContent='center' alignItems='center' style={{ marginBottom: '25px' }}>
           <ul>
-            <Typography gutterBottom>
-              <li>Write some code to consume the json hosted on the above web service.</li>
-            </Typography>
-            <Typography gutterBottom>
-              <li>Output a list of all the cats in alphabetical order under a heading of the gender of their owner.</li>
-            </Typography>
-            <Typography gutterBottom>
-              <li>Output must be presentable on a web browser.</li>
-            </Typography>
-            <Typography gutterBottom>
-              <li>Submissions will only be accepted via GitHub or Bitbucket.</li>
-            </Typography>
+            <li>
+              <Typography gutterBottom>
+                Write some code to consume the json hosted on the above web service.
+              </Typography>
+            </li>
+            <li>
+              <Typography gutterBottom>
+                Output a list of all the cats in alphabetical order under a heading of the gender of their owner.
+              </Typography>
+            </li>
+            <li>
+              <Typography gutterBottom>
+                Output must be presentable on a web browser.
+              </Typography>
+            </li>
+            <li>
+              <Typography gutterBottom>
+                Submissions will only be accepted via GitHub or Bitbucket.
+              </Typography>
+            </li>
           </ul>
         </Grid>
+
         <Grid container justifyContent='center' alignItems='center'>
-          <Button variant='contained' color='error'>Show solution</Button>
+          <Button variant={showSolution ? 'outlined' : 'contained'} color='error' onClick={() => setShowSolution(!showSolution)}>
+            {showSolution ? 'Hide solution' : 'Show solution'}
+          </Button>
         </Grid>
-        <DisplayContent />
+
+        <DisplayContent
+          filteredMaleArray={filteredMaleArray}
+          filteredFemaleArray={filteredFemaleArray}
+          showSolution={showSolution}
+        />
+
+        {showSolution && (
+          <Grid container justifyContent='center' direction='column' alignItems='center' spacing={2}>
+            <Grid item>
+              <TextField
+                label="Filter cats here"
+                variant="outlined"
+                color='error'
+                onChange={e => setFilteredData(e.target.value)}
+                value={filteredData}
+              />
+            </Grid>
+            <Grid item>
+              <Button variant='outlined' color='error' onClick={() => setFilteredData('')}>Clear filter</Button>
+            </Grid>
+          </Grid>
+        )}
       </Container>
     </>
   )
--- src/components/DisplayContent.js
+++ src/components/DisplayContent.js
@@ -1,84 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 import { Typography, Grid } from '@mui/material';
 import Man2Icon from '@mui/icons-material/Man2';
 import Woman2Icon from '@mui/icons-material/Woman2';
 
-const DisplayContent = () => {
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const URL = 'https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json'
-            const { data: result } = await axios.get(URL)
-
-            //To filter only people who have pets, as not everyone has pets
-            const peopleWithPets = result.filter(obj => obj.pets)
-
-            setData(peopleWithPets)
-        }
-
-        fetchData()
-    }, [])
-
-
-    //As per the challenge requirement - to list sorted cat names under the gender of their owner
-    const populateCatsBasedOnGender = (array, gender) => {
-        let temp = []
-
-        const genderArray = array.filter(el => el.gender === gender)
-
-        genderArray.forEach(el => {
-            el.pets.forEach(pet => {
-                if (pet.type === 'Cat') temp.push(pet)
-            })
-        })
-
-        return temp
-    }
-
-
-    //catsObjWithMaleOwner and catsObjWithFemaleOwner are now populated: [ {cat object}, {cat object}, {cat object} ]
-    const catsObjWithMaleOwner = populateCatsBasedOnGender(data, 'Male')
-    const catsObjWithFemaleOwner = populateCatsBasedOnGender(data, 'Female')
-
-    //To produce an array of cat string names and sort them alphabetically 
-    const createAndSortCats = arr => {
-        let tempArray = []
-        arr.forEach(el => {
-            tempArray.push(el.name)
-        })
-        return tempArray.sort()
-    }
-
-    //Now we get an array of cat names that have been sorted e.g. [ "Garfield", "Jim", "Max", "Tom" ]
-    const catsMaleArray = createAndSortCats(catsObjWithMaleOwner)
-    const catsFemaleArray = createAndSortCats(catsObjWithFemaleOwner)
+const DisplayContent = ({ filteredMaleArray, filteredFemaleArray, showSolution }) => {
 
     //Turn the array into a JSX element, so that we can render 
-    const displayMaleCats = catsMaleArray.map(cat => (
+    const displayMaleCats = filteredMaleArray.map(cat => (
         <li key={cat}>{cat}</li>
     ))
 
-    const displayFemaleCats = catsFemaleArray.map(cat => (
+    const displayFemaleCats = filteredFemaleArray.map(cat => (
         <li key={cat}>{cat}</li>
     ))
+
 
     return (
-        <Grid container justifyContent='space-around' style={{ marginTop: '25px' }}>
-            <Grid item>
-                <Typography variant='h5' align='center'>
-                    Male{'  '}<Man2Icon />
-                </Typography>
-                <ul style={{ padding: 0 }}>{displayMaleCats}</ul>
-            </Grid>
-            <Grid item>
-                <Typography variant='h5' align='center'>
-                    Female{'  '}<Woman2Icon />
-                </Typography>
-                <ul style={{ padding: 0 }}>{displayFemaleCats}</ul>
-            </Grid>
-        </Grid>
+        <>
+            {showSolution && (<>
+                <Grid container justifyContent='space-around' style={{ marginTop: '25px' }}>
+                    <Grid item>
+                        <Typography variant='h5' align='center'>
+                            Male{'  '}<Man2Icon />
+                        </Typography>
+                        <ul style={{ padding: 0 }}>
+                            {displayMaleCats}
+                        </ul>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant='h5' align='center'>
+                            Female{'  '}<Woman2Icon />
+                        </Typography>
+                        <ul style={{ padding: 0 }}>
+                            {displayFemaleCats}
+                        </ul>
+                    </Grid>
+                </Grid>
+            </>)}
+        </>
     )
 }
 
